4d87326048668130e2e11b25e158423e08841b80
opagani <opagani@zillowgroup.com>
2018-03-21 12:50:37 -0700

    adding about page and navigation

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const AboutPage = () => (
+  <div>
+    <h1>About Us</h1>
+    <p>Oscar Pagani is a Senior Sofware Engineer at Zillow in San Francisco.</p>
+    <Link to="/">Go back to the homepage</Link>
+  </div>
+)
+
+export default AboutPage
