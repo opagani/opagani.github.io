f2ace3bfcd555e15ed3bb67c2e7999aaf0c32b53
opagani <opagani@zillowgroup.com>
2018-03-21 15:07:30 -0700

    importing assets

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import Link from 'gatsby-link'
 
+import logo from '../../images/logo.svg'
+
+console.log(logo)
+
 const Header = () => (
   <div
     style={{
@@ -23,7 +27,7 @@ const Header = () => (
             textDecoration: 'none',
           }}
         >
-          Gatsby
+          <img src={logo} alt='logo' />
         </Link>
       </h1>
     </div>
