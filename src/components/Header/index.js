81a4c353f2ee11e20bb8d0784cafb015e793f0b6
opagani <opagani@zillowgroup.com>
2018-04-09 11:52:16 -0700

    Added new blog post

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -27,6 +27,27 @@ const HeaderContainer = styled.div`
   padding: 1.45rem 1.0875rem;
   position: relative;
   z-index: 2;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const MainNav = styled.nav`
+  ul {
+    list-style: none;
+    display: flex;
+    li {
+      margin-left: 10px;
+      font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu,
+                    Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+      a {
+        text-decoration: none;
+        color: #fff;
+        &:hover {
+          border-bottom: 3px solid #524763;
+        }
+      }
+    }
+  }
 `;
 
 export default class Header extends Component {
@@ -81,7 +102,7 @@ export default class Header extends Component {
               <img src={logo} alt='logo' />
             </Link>
           </h1>
-          <nav>
+          <MainNav>
             <ul>
               <li>
                 <Link to="/">Home</Link>
@@ -90,14 +111,15 @@ export default class Header extends Component {
                 <Link to="/about">About</Link>
               </li>
             </ul>
-          </nav>
+          </MainNav>
         </HeaderContainer>
         <Img style={{
             position: 'absolute',
             left: 0,
             top: 0,
             width: '100%',
-            height: '100%'
+            height: '100%',
+            opacity: 0.3
           }}
           sizes={data.background.sizes} 
         />
